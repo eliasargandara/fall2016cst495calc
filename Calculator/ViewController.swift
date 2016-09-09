ed56b7758eb61a80cd4ebafd9350c41006dc9f2b
Eliasar Gandara <elgandara@csumb.edu>
2016-09-09 09:50:00 -0700

    Final commit for assignment 1

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -12,10 +12,14 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var display: UILabel!
     
+    let π = M_PI
     var userIsInTheMiddleOfTypingANumber: Bool = false
+    var isDecimalAlreadyInNumber: Bool = false
+    
+    var operandStack = Array<Double>()
 
     @IBAction func operate(sender: UIButton) {
-        let operation = sender.currentTitle!;
+        let operation = sender.currentTitle!
         if userIsInTheMiddleOfTypingANumber {
             enter()
         }
@@ -25,6 +29,8 @@ class ViewController: UIViewController {
         case "+": performOperation { $0 + $1 }
         case "-": performOperation { $1 - $0 }
         case "√": performOperation { sqrt($0) }
+        case "sin": performOperation { sin($0) }
+        case "cos": performOperation { cos($0) }
         default: break
         }
     }
@@ -41,8 +47,6 @@ class ViewController: UIViewController {
         }
     }
     
-    var operandStack = Array<Double>();
-    
     @IBAction func appendDigit(sender: UIButton) {
         let digit = sender.currentTitle!
         if userIsInTheMiddleOfTypingANumber {
@@ -54,14 +58,46 @@ class ViewController: UIViewController {
         }
     }
     
+    @IBAction func appendDecimal() {
+        if !isDecimalAlreadyInNumber {
+            display.text = (display.text!) + "."
+            isDecimalAlreadyInNumber = true
+        }
+    }
+    
+    @IBAction func insertSymbolicValue(sender: UIButton) {
+        let symbol = sender.currentTitle!
+        switch symbol {
+        case "π": insertValueIntoKeypad(π)
+        default: break;
+        }
+    }
+    
+    private func insertValueIntoKeypad(value: Double) {
+        display.text = "\(value)"
+        userIsInTheMiddleOfTypingANumber = false
+        isDecimalAlreadyInNumber = false
+    }
+    
     @IBAction func enter() {
         userIsInTheMiddleOfTypingANumber = false
+        isDecimalAlreadyInNumber = false
         operandStack.append(displayValue);
         print("operandStack = \(operandStack)")
     }
     
+    @IBAction func clear() {
+        userIsInTheMiddleOfTypingANumber = false
+        isDecimalAlreadyInNumber = false
+        operandStack = Array<Double>()
+        display.text = "0"
+    }
+    
     var displayValue: Double {
         get {
+            if display.text! == "." {
+                display.text = "0"
+            }
             return NSNumberFormatter().numberFromString(display.text!)!.doubleValue
         }
         set {
